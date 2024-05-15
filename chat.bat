4db40d891e6ca961fa9007fb0f06996a9f7f57e4
ExotAni <54.hadjiev@mail.ru>
2024-05-15 21:38:38 +0300

    Исправил несколько костылей на другие костыли

--- chat.bat
+++ chat.bat
@@ -3,11 +3,14 @@ color 0a
 chcp 1251
 title Ne1nyChat
 
-if not exist \\exotani\FreeForAll\Ne1nyChat color 0c && cls && echo No Radmin connection... && pause>nul && exit
+set nick=Malki
+set history_direct=\\exotani\FreeForAll\Ne1nyChat
 
-if not exist \\exotani\FreeForAll\Ne1nyChat\mhistory.txt (
-    echo Ne1nyChat starting here ^<3>\\exotani\FreeForAll\Ne1nyChat\mhistory.txt
-    echo. >>\\exotani\FreeForAll\Ne1nyChat\mhistory.txt
+if not exist %history_direct% color 0c && cls && echo No Radmin connection... && pause>nul && exit
+
+if not exist %history_direct%\mhistory.txt (
+    echo Ne1nyChat starting here ^<3>%history_direct%\mhistory.txt
+    echo. >>%history_direct%\mhistory.txt
 )
 cls
 
@@ -18,8 +21,6 @@ timeout /t 1 /nobreak>nul
 echo.
 set massage= 
 set /p massage=">>>"
-set custom_time=%time:~0,8%
-set custom_date=[Date: %date%]
-find "%custom_date%" \\exotani\FreeForAll\Ne1nyChat\mhistory.txt>nul || echo %custom_date%>>\\exotani\FreeForAll\Ne1nyChat\mhistory.txt
-echo [%custom_time%] Malki: %massage%>>\\exotani\FreeForAll\Ne1nyChat\mhistory.txt
+find "[Date: %date%]" %history_direct%\mhistory.txt>nul || echo [Date: %date%]>>%history_direct%\mhistory.txt
+echo [%time:~0,8%] %nick%: %massage%>>%history_direct%\mhistory.txt
 goto main
--- massagecheck.bat
+++ massagecheck.bat
@@ -1,10 +1,10 @@
 echo off
 cls
-type \\exotani\FreeForAll\Ne1nyChat\mhistory.txt
+type %history_direct%\mhistory.txt
 
 :massagecheck
-fc \\exotani\FreeForAll\Ne1nyChat\mhistory.txt mhistory_b.txt>nul && goto massagecheck
-type \\exotani\FreeForAll\Ne1nyChat\mhistory.txt > mhistory_b.txt
+fc %history_direct%\mhistory.txt mhistory_b.txt>nul && goto massagecheck
+type %history_direct%\mhistory.txt > mhistory_b.txt
 cls
-type \\exotani\FreeForAll\Ne1nyChat\mhistory.txt
+type %history_direct%\mhistory.txt
 goto massagecheck
